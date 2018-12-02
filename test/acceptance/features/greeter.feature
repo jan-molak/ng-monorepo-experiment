e8a07ee7d61d3bbb64c2b13fa97386fdbfd835fa
Jan Molak <jan.molak@smartcodeltd.co.uk>
2018-12-02 23:41:15 +0000

    Acceptance tests run against the core

--- test/acceptance/features/greeter.feature
+++ test/acceptance/features/greeter.feature
@@ -4,4 +4,4 @@ Feature: Greeter
 
     Given my name is Jan
     When the Greeter greets me
-    Then it should say "Hi Jan!"
+    Then it should say "Hello Jan!"
--- test/acceptance/features/step_definitions/greeter.steps.ts
+++ test/acceptance/features/step_definitions/greeter.steps.ts
@@ -1,15 +1,22 @@
+import { Greeter } from '@experimental/core';
 import { StepDefinitions } from 'cucumber';
+import * as assert from 'assert';
 
 export = function(this: StepDefinitions) {
-    this.Given(/^my name is (.*)$/, function (name: string) {
+    const greeter = new Greeter();
 
+    let myName: string,
+        greeting: string;
+
+    this.Given(/^my name is (.*)$/, function (name: string) {
+        myName = name;
     });
 
     this.When(/^the Greeter greets me$/, function () {
-
+        greeting = greeter.greet(myName);
     });
 
     this.Then(/^it should say ['"](.*)['"]$/, function (expectedGreeting: string) {
-
+        assert.equal(greeting, expectedGreeting);
     });
 };
